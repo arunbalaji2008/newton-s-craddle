afbb377836c43c09f093733a9a6f97bb2edb9cbc
Mr.Misdreavus <95179527+arunbalaji2008@users.noreply.github.com>
2022-03-16 19:05:18 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,32 @@
+class roof
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(200,50, 1000, h , options);
+		World.add(world, this.body);
+ 		
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER);
+			strokeWeight(4);
+			fill(128,128,128);
+			rect(this.body.position.x,this.body.position.y,this.w, this.h);
+			pop();
+			
+	}
+
+}
